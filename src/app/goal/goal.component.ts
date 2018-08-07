c49ac630c81eabe7711cec56efc5913ff91de781
EugeneZnm <eugenenzioki@gmail.com>
2018-08-07 22:31:29 +0300

    change

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -12,7 +12,7 @@ export class GoalComponent implements OnInit {
     new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son'),
     new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
     new Goal(3, 'Get new Phone Case', 'Diana has her birthday coming up soon'),
-    new Goal(4, 'Get Dog Food', 'Pupper likes expensive sancks'),
+    new Goal(4, 'Get Dog Food', 'Pupper likes expensive snacks'),
     new Goal(5, 'Solve math homework', 'Damn Math'),
     new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord'),
 ];
@@ -27,5 +27,12 @@ toggleDetails(index) {
 
   ngOnInit() {
   }
+
+  // completeGoal function where javascript splice function is used to remove complete goal and delete from array
+completeGoal(isComplete, index) {
+  if (isComplete) {
+      this.goals.splice(index, 1);
+      }
+      }
 
 }
